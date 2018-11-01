74c91239b3f792f5034749b1883ba62772b90628
JohPa8696 <anynomous@ano.com>
2018-11-01 18:59:48 +1300

    added button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 // This is a functional component
 const AlbumDetail = ({ album }) => {
@@ -30,6 +31,9 @@ const AlbumDetail = ({ album }) => {
         <Image
           style={imageStyle}
           source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button/>
       </CardSection>
     </Card>
   );
